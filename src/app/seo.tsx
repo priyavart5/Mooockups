4b2372bb5535351ccd6571046facf070f8aa2240
priyavart10 <priyavart705@gmail.com>
2024-11-19 17:32:16 +0530

    minor changes

--- src/app/seo.tsx
+++ src/app/seo.tsx
@@ -2,6 +2,6 @@ import favicon from '../../public/favicon.ico';
 
 export const metadata = {
     title: "Mockups By PV",
-    description: "All New Mockup Editors By Dev.",
+    description: "A revolutionary mockup editors by a developer & designer.",
     icons: {favicon},
 };
